0d8992518d81294f49064e54919ba9d933c12318
Marcelo Martim Marques <marcelo.martim@gmail.com>
2015-04-18 13:19:28 -0300

    add new script to move camera files

---
+++ bin/move_media.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+if [ -d /media/1/cameras/camera1 ]; then
+  if [ -d /media/2/cameras/camera1 ]; then
+    sudo find /media/2/cameras/camera1 -mmin +10 -exec mv -f {} /media/1/cameras/camera1 \;
+  fi
+fi
