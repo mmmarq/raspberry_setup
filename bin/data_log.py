9f8f1eb34f30b410983b499fd156888ce536373a
Marcelo Martim Marques <marcelo.martim@gmail.com>
2015-06-04 20:51:49 -0300

    Update crontab and update data_log.py script

--- bin/data_log.py
+++ bin/data_log.py
@@ -3,7 +3,6 @@
 
 #Import needed libs
 import datetime
-import serial
 import time
 import sys
 import subprocess
@@ -12,13 +11,7 @@ from time import localtime, strftime
 
 def main():
   EXE = "sudo /media/2/code/raspberry_lcd/dht11"
-  LOG = "/media/2/log/new_local_data.log"
-
-  #Open log file to write
-  f = open(LOG,'a')
-  #Get date
-  date = strftime("%Y,%m,%d,%H:%M", localtime())
-
+  LOG = "/media/2/log/local_data.log"
 
   while True:
     try:
@@ -27,25 +20,19 @@ def main():
       output=subprocess.check_output(EXE, shell=True)
       #Parse local data
       ldata = output.split()
+      #Get date
+      date = strftime("%Y,%m,%d,%H:%M", localtime())
+      #Open log file to write
+      f = open(LOG,'a')
+      #Save data into log file
+      f.write(date + ',' + ldata[1] + ',' + ldata[0] + '\n')
+      #Close log file
+      f.close()
       #Stop loop
       break
     except:
       #If sensor reading fail, try again
       continue
-
-  ndata = ""
-  with serial.Serial('/dev/ttyUSB0', 9600, timeout=1) as ser:
-    while ndata == "":
-      ser.write('?')
-      ndata = ser.readline().rstrip()
-      time.sleep(2)
-    ndata = ndata.split(',')
-
-  #Save data into log file
-  f.write(date + ',' + ldata[1] + ',' + ldata[0] + ',' + ndata[0] + ',' + ndata[1] + ',' + ndata[3] + '\n')
-
-  #Close log file
-  f.close()
 
 if __name__ == '__main__':
    main()
