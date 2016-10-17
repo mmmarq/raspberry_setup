e3adc590fa6d6a7684245f5cf6dfd9e7acccd55b
Marcelo Martim Marques <marcelo.martim@gmail.com>
2016-10-17 09:46:50 -0200

    Improve motion_uploader.sh lock file verification and make usb disk mount by device UUID instead of /dev/sdXX pointer

--- bin/motion_uploader.sh
+++ bin/motion_uploader.sh
@@ -15,8 +15,17 @@ cleanup()
 
 
 if [ -f $TEMP_FILE ]; then
-  echo "Motion uploader running. Do no run another task in parallel"
-  exit 0
+  echo "Motion uploader lock file found!"
+  CURR_AGE=`date +%s`
+  LOCK_AGE=`stat -L --format %Y $TEMP_FILE`
+  AGE=`expr $CURR_AGE - $LOCK_AGE`
+  if [ $AGE -lt 1800 ]; then
+    echo "Motion uploader running. Do no run another task in parallel!"
+    exit 0
+  else
+    echo "Motion uploader lock file looks like too old. Lets remove it and run again!"
+    echo $$ > $TEMP_FILE
+  fi
 else
   echo $$ > $TEMP_FILE
 fi
