6f8e7cb46693fe1f4a7ed395386ebebe00febacd
Marcelo Marques <marcelo.martim@gmail.com>
2016-10-29 18:15:11 -0200

    Add barometer data

--- bin/data_log.py
+++ bin/data_log.py
@@ -18,22 +18,23 @@ def main():
     # Read data from sensor
     response = urllib2.urlopen(IP, timeout=2)
     html = response.read()
-    temp,humid,light,alarm,rasp = html.split()
+    temp,humid,pres,light,alarm,rasp = html.split()
 
     lTemp = "{0:0.1f}".format(float(temp))
     lHumid =  "{0:0.1f}".format(float(humid))
+    lPres =  "{0:0.1f}".format(float(pres))
     #Get date
     date = strftime("%Y,%m,%d,%H:%M", localtime())
     #Open log file to write
     f = open(LOG,'a')
     #Save data into log file
-    f.write(date + ',' + lTemp + ',' + lHumid + ',' + light  + '\n')
+    f.write(date + ',' + lTemp + ',' + lPres + ',' + lHumid + ',' + light  + '\n')
     #Close log file
     f.close()
     #Open database connection
     con = sqlite3.connect(DB)
     #Log data on database
-    mySql = 'INSERT INTO weather_data VALUES(' + str(int(time())) + ',' + lTemp + ',' + lHumid + ')'
+    mySql = 'INSERT INTO weather_data VALUES(' + str(int(time())) + ',' + lTemp + ',' + lHumid + ',' + lPres + ')'
     with con:
       cur = con.cursor()
       cur.execute(mySql)
