663f754b614203124e9779af42d99dd1577be7e5
Marcelo Martim Marques <marcelo.martim@gmail.com>
2016-12-15 13:06:46 -0200

    Move file storage from Google Drive to Amazon Cloud Drive

--- bin/motion_uploader.sh
+++ bin/motion_uploader.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-UPLOADER="/mnt/code/motion-uploader/uploader.py"
+# REFERENCE
+# http://acd-cli.readthedocs.io/en/latest/usage.html
+# http://xmodulo.com/access-amazon-cloud-drive-command-line-linux.html
+
+UPLOADER="/usr/local/bin/acd_cli -nl"
 TEMP_FILE="/tmp/motion_uploader.tmp"
 
 trap cleanup 1 2 3 6 9 15
@@ -30,11 +34,18 @@ else
   echo $$ > $TEMP_FILE
 fi
 
+# REFRESH CACHE
+#$UPLOADER sync
+
+# CREATE CURRENT DATA FOLDER
+CURR_DATE=`date +%Y-%m-%d`
+$UPLOADER mkdir --parents /Videos/Motion/$CURR_DATE
+
 FILES=`find /mnt/media/cameras/camera1 -mmin +10`
 
 for file in $FILES; do
-  $UPLOADER /home/pi/uploader.cfg $file
+  FILE_DATE=`echo $file | sed 's/-[0-9]*_.*$//g' | sed 's/^.*\///g'`
+  $UPLOADER ul --force --remove-source-files --quiet $file /Videos/Motion/$FILE_DATE
 done
 
 rm -f $TEMP_FILE
-
